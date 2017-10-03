32c5c5e6412346b9e7ab7169fe1693f8d2818022
MacOS <phuochau409@gmail.com>
2017-10-03 13:00:19 +0700

    Update
    Cc

---
+++ PikachuMario/Assets/Scripts/movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float speed, jumpSpeed;
+	public Rigidbody2D player;
+	public Animator anim;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+		Jump ();
+
+		anim.SetFloat ("speed", Mathf.Abs(player.velocity.x));
+		anim.SetFloat ("speed y", player.velocity.y);
+		anim.SetBool ("grounded", grounded);
+		
+	}
+	void Move()
+	{
+		float dir_x = Input.GetAxisRaw ("Horizontal");
+		player.velocity = new Vector2 (speed * dir_x, player.velocity.y);
+
+		if (dir_x != 0) {
+			Vector3 temp = transform.localScale;
+			temp.x = dir_x;
+			transform.localScale = temp;
+		}
+	}
+
+	void Jump()
+	{
+		if (grounded) {
+			
+			float dir_y = Input.GetAxisRaw ("Vertical");
+			if (dir_y != 0f) {
+				grounded = false;
+				player.velocity = new Vector2 (player.velocity.x, jumpSpeed * dir_y);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		grounded = true;
+	}
+}
